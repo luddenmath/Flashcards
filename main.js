baaf073f2820152dd51783140a3d59729cf74a4d
luddenmath <luddenjacob2@gmail.com>
2026-08-02 00:15:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (async function(){
-//9
+//10
 if(window.studentFlashcardOpen){
     document.getElementById("studentFlashcardOverlay")?.remove();
     window.studentFlashcardOpen=false;
@@ -1097,7 +1097,7 @@ imagesDiv.appendChild(cell);
 
 requestAnimationFrame(()=>{
 
-const cols = 5;
+const cols = 15;
 
 const rows = Math.ceil(
     studentsForGame.length / cols
